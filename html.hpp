85623575c7a1a8e27aff5ac03f2a0f6676ee6118
adnKsharp <darkmark404@protonmail.com>
2022-08-05 20:53:18 -0600

    init html pages for cpp

--- html.hpp
+++ html.hpp
@@ -1,4 +1,23 @@
+String CSS()
+{
+    String css = "";
+    return css;
+}
+
+
 String Hindex()
+{
+    String html = "";
+    return html;
+}
+
+String Hindexi()
+{
+    String html = "";
+    return html;
+}
+
+String Hindex0()
 {
     String html = "";
     return html;
